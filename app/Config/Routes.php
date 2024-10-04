4b970d5d4583d5e058c2c40d5fcaf21e39fbaca5
Ras <siregar.riyando@gmail.com>
2024-10-04 11:04:20 +0800

    Added routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'About::index');
+$routes->get('/about/test', 'About::test');
+$routes->get('/admin/dashboard', 'Admin\Dashboard::index');
